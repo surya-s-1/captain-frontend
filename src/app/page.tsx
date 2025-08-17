9008e5ee4fe8cadeccae68343ae5a895e1b39f97
Surya S <suryah2806@gmail.com>
2025-08-17 21:37:02 +0530

    minor styling change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     const appUser = useSelector((state: RootState) => state.user)
 
     return (
-        <div className='flex w-full h-full items-center justify-center bg-primary text-color-primary/50 text-7xl pb-20'>
+        <div className='flex w-full h-full items-center justify-center bg-primary text-color-primary/50 text-7xl pb-52'>
             Hello,<br />
             {appUser.name}!
         </div>
