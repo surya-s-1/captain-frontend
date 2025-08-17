e58ff172e0cd3646ab0d4449cbf6b67a237d165f
Surya S <suryah2806@gmail.com>
2025-08-17 20:57:56 +0530

    formatting

--- src/lib/slices/user.ts
+++ src/lib/slices/user.ts
@@ -1,42 +1,42 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 interface UserState {
-  uid: string | null
-  name: string | null
-  email: string | null
-  loading: boolean
+    uid: string | null
+    name: string | null
+    email: string | null
+    loading: boolean
 }
 
 const initialState: UserState = {
-  uid: null,
-  name: 'User',
-  email: null,
-  loading: true,
+    uid: null,
+    name: 'User',
+    email: null,
+    loading: true,
 }
 
 export const userSlice = createSlice({
-  name: 'user',
-  initialState,
-  reducers: {
-    setUser: (
-      state,
-      action: PayloadAction<{ uid: string; name: string; email: string }>
-    ) => {
-      state.uid = action.payload.uid
-      state.name = action.payload.name
-      state.email = action.payload.email
-      state.loading = false
-    },
-    clearUser: (state) => {
-      state.uid = null
-      state.name = null
-      state.email = null
-      state.loading = false
-    },
-    setLoading: (state, action: PayloadAction<boolean>) => {
-      state.loading = action.payload
-    },
-  }
+    name: 'user',
+    initialState,
+    reducers: {
+        setUser: (
+            state,
+            action: PayloadAction<{ uid: string; name: string; email: string }>
+        ) => {
+            state.uid = action.payload.uid
+            state.name = action.payload.name
+            state.email = action.payload.email
+            state.loading = false
+        },
+        clearUser: (state) => {
+            state.uid = null
+            state.name = null
+            state.email = null
+            state.loading = false
+        },
+        setLoading: (state, action: PayloadAction<boolean>) => {
+            state.loading = action.payload
+        },
+    }
 })
 
 export const {
--- src/lib/store.ts
+++ src/lib/store.ts
@@ -9,20 +9,20 @@ const rootReducer = combineReducers({
 })
 
 const persistConfig = {
-  key: 'root',
-  storage
+    key: 'root',
+    storage
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE']
-      }
-    })
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE']
+            }
+        })
 })
 
 export const persistor = persistStore(store)
