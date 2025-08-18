4762e809e614070afe684e349aecf9662e48aa68
Surya S <suryah2806@gmail.com>
2025-08-18 18:40:39 +0530

    removed storage bucket

--- src/lib/firebase/index.ts
+++ src/lib/firebase/index.ts
@@ -5,7 +5,6 @@ const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FB_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FB_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FB_STORAGE_BUCKET,
     appId: process.env.NEXT_PUBLIC_FB_APP_ID,
 }
 
