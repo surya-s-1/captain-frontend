5d5bdc9bb42fc2f50653ea1c02b0719c13e27a5c
Surya S <suryah2806@gmail.com>
2025-08-18 10:44:55 +0530

    minor fix

--- src/components/auth-gaurd.tsx
+++ src/components/auth-gaurd.tsx
@@ -18,7 +18,7 @@ export default function AuthGuard({ children }: { children: React.ReactNode }) {
     }, [uid, loading, pathname, router])
 
     if (loading) {
-        return <div className="flex items-center justify-center h-screen">Loading...</div>
+        return <div className='flex items-center justify-center h-screen'>Loading...</div>
     }
 
     return <>{children}</>
