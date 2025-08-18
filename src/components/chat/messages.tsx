de0e232d7a7d4c14d7f9d227515baf6440325b7c
Surya S <suryah2806@gmail.com>
2025-08-18 12:01:41 +0530

    showing hello message in chat page

--- src/components/chat/messages.tsx
+++ src/components/chat/messages.tsx
@@ -1,10 +1,19 @@
 'use client'
 
 import React from 'react'
+import { useSelector } from 'react-redux'
+
+import { RootState } from '@/lib/store'
 
 export default function ChatMessages() {
+    const appUser = useSelector((state: RootState) => state.user)
+    
     return (
         <div className='w-full flex-1 space-y-2 overflow-y-auto scrollbar-hide'>
+            <div className='flex w-full h-full items-center justify-center bg-primary text-color-primary/50 text-7xl pb-52'>
+                Hello,<br />
+                {appUser.name}!
+            </div>
         </div>
     )
 }
