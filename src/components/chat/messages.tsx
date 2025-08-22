dbc0adfddfbd55293d6ba4d7036c34f4a6f1a4c3
Surya S <suryah2806@gmail.com>
2025-08-22 11:42:23 +0530

    showing scroll bar to messages

--- src/components/chat/messages.tsx
+++ src/components/chat/messages.tsx
@@ -79,7 +79,7 @@ export default function ChatMessages() {
                     )}
                 </div>
             ) : (
-                    <div className='flex flex-col gap-2 p-4 scrollbar-hide' style={{ flex: 1, overflowY: 'auto' }}>
+                    <div className='flex flex-col gap-2 p-4 scrollbar' style={{ flex: 1, overflowY: 'auto' }}>
                         {messages.map((msg) => {
                             const cleanText = msg.role === 'model' ? msg.text.replaceAll('[text]:', '') : msg.text
 
