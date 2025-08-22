3fc5f7931a2fcbeabadc57c5e5b322354e880e1d
Surya S <suryah2806@gmail.com>
2025-08-22 17:17:03 +0530

    added firebase hosting config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    devIndicators: false
+    devIndicators: false,
+    output: 'export',
+    images: {
+        unoptimized: true
+    }
 }
 
 export default nextConfig
