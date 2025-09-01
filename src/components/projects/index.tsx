e8206d65d9f8e397ebebcf3889e8e9576c02f1b4
Surya S <suryah2806@gmail.com>
2025-09-01 20:25:51 +0530

    updated new button in projects page

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -41,27 +41,18 @@ export default function Projects() {
 
     return (
         <div className='flex flex-col items-start p-8 w-full'>
-            <div className='flex justify-end mb-4 w-full'>
-                <a
-                    href='/projects/new'
-                    className='bg-primary-contrast text-color-primary-contrast hover:bg-primary-contrast/80 transition-colors cursor-pointer px-4 py-2 rounded-md'
-                >
-                    New Project
+            <h1 className='text-2xl font-bold mb-4'>My Projects</h1>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full'>
+                {projects.map((project, idx) => (
+                    <div key={idx} className='border min-h-40 cursor-pointer p-4 rounded-md shadow-md'>
+                        <h2 className='text-xl font-semibold'>{project.project_name}</h2>
+                        <p className='text-gray-600'>{project.tool}</p>
+                    </div>
+                ))}
+                <a href='/projects/new' className='flex items-center justify-center border min-h-40 cursor-pointer p-4 rounded-md shadow-md'>
+                    <p className='text-gray-600'>+ New Project</p>
                 </a>
             </div>
-            <h1 className='text-2xl font-bold mb-4'>My Projects</h1>
-            {projects.length === 0 ? (
-                <p>No projects found. Create a new one!</p>
-            ) : (
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full'>
-                    {projects.map((project, idx) => (
-                        <div key={idx} className='border p-4 rounded-md shadow-md'>
-                            <h2 className='text-xl font-semibold'>{project.project_name}</h2>
-                            <p className='text-gray-600'>{project.tool}</p>
-                        </div>
-                    ))}
-                </div>
-            )}
         </div>
     )
 }
