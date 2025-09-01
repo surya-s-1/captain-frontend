c9e4304eb4c0440eb288d1908bc09533445926aa
Surya S <suryah2806@gmail.com>
2025-09-01 20:32:10 +0530

    checks for projectId and version upon initial load

--- src/components/projects/details.tsx
+++ src/components/projects/details.tsx
@@ -1,5 +1,20 @@
 'use client'
 
+import { useEffect } from 'react'
+import { useSearchParams, useRouter } from 'next/navigation'
+
 export default function ProjectDetails() {
+    const searchParams = useSearchParams()
+    const router = useRouter()
+
+    useEffect(() => {
+        const projectId = searchParams.get('projectId')
+        const version = searchParams.get('version')
+
+        if (!projectId || !version) {
+            router.push('/projects')
+        }
+    }, [searchParams, router])
+
     return <></>
 }
