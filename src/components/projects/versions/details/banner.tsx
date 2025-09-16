ac5ed576c809a75b1c72c05775a91e690b0a8577
Surya S <suryah2806@gmail.com>
2025-09-16 20:36:39 +0530

    a small fix

--- src/components/projects/versions/details/banner.tsx
+++ src/components/projects/versions/details/banner.tsx
@@ -228,7 +228,7 @@ export default function DetailsBanner({ status }) {
                                 ))}
                             </ul>
                         </div>}
-                    {versionFiles.length > 0 && (
+                    {versionFiles.length > 0 && status != 'CREATED' && (
                         <div className='flex flex-col gap-1'>
                             <span>Updated Requirements?</span>
                             <button
